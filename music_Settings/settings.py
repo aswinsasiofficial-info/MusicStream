becdf8e097389bb80eccdecf7417f39260431032
aswinsasiofficial-info <aswinsasiofficial.in@gmail.com>
2026-05-17 21:11:33 +0530

    5

--- music_Settings/settings.py
+++ music_Settings/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-tdyjx^_qlgciopg_9dv7x!q&hav(yf+&h27i$z6)z-1ea5(pb!')
 DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=lambda v: [s.strip() for s in v.split(',')])
 
 INSTALLED_APPS = [
     'django.contrib.admin',
